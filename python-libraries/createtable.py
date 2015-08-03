3fab6f2be73d00cbe498658f585b3d1cb1570786
harshdattani <harsh.dattani@hotmail.com>
2015-08-03 23:25:37 +0530

    Adding Preety Python Library Demo

---
+++ python-libraries/createtable.py
@@ -0,0 +1,7 @@
+from prettytable import PrettyTable
+table = PrettyTable(["Name", "Marks"])
+table.add_row(["Harsh", 100])
+table.add_row(["Manoranjan", 101])
+table.add_row(["Manish", 110])
+table.add_row(["Amitabh", 35])
+print table
